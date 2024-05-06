c659ad05f92f442bc7ce8dea3ddcbdb479afac3c
Aman verma <119755989+aman12z@users.noreply.github.com>
2024-05-07 00:08:59 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,24 @@
+package com.basic.java;
+
+import java.util.Scanner;
+public class addition {
+    public static void main(String[] args ) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("first no");
+        int A = sc.nextInt();
+
+        System.out.println("second no");
+
+        int B = sc.nextInt();
+
+        int C = A+B;
+
+        System.out.println("total :  "+ C);
+
+
+
+
+
+
+    }
+}
